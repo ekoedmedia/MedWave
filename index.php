1cb793a88026f3de4174c4bae1a9422e077e7431
Jeremy Smereka <ekeodmedia@gmail.com>
2013-02-19 18:51:47 -0700

    Started Development of Login and ORM
    Login system skeleton has begun
    ORM Basic NON-WORKING version has begun.

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MedWave | Waves for the Future</title>
+</head>
+<body>
+	<div class="wrapper_content">
+		<header></header>
+		<div class="content">
+			This is just some content to showcase that this is working.
+		</div>
+		<footer></footer>
+	</div>
+</body>
+</html>
---
+++ system/orm/mapper.php
@@ -0,0 +1,60 @@
+<?php
+
+$data = array("user_name" => "JEREMY")
+
+namespace MedWave\System\ORM {
+
+	class Mapper {
+
+		/**
+		 *
+		 *
+		 *
+		 *
+		 */
+		public function __construct($table, Array $data)
+		{	
+			try {
+				$determinedObj = $this->determineObj($table);
+				$obj = new $determinedObj();
+				foreach ($data AS $key => $value) {
+					$obj->set$key($value);
+				}
+
+				return $obj;
+			} catch (InvalidObjectException $e){
+				##TODO: This probably isn't the correct function call for this.
+				$e->get_error();
+			}
+		}
+
+
+		/**
+		 *
+		 *
+		 *
+		 */
+		private function determineObj($table) 
+		{	
+			$thisObject = null;
+			##TODO: Add in SQL Show Tables from DBName
+			$result = mysql_query('SHOW TABLE FROM DBNAME')
+			foreach ($result AS $tableName) {
+				if ($tableName == $table)
+					$thisObject = $tableName;
+			}
+
+			if ($thisObject == null) {
+				##TODO: Look into SPL Exceptions
+				throw new InvalidObjectException(); 
+			} else {
+				return $thisObject;
+			}
+		}
+
+	}
+
+}
+
+
+
