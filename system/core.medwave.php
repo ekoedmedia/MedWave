741e76d74d7234743d11e7b84be3e1e9fe4a05c3
ekoedmedia <ekoedmedia@gmail.com>
2013-02-28 15:25:09 -0700

    Removed login/orm from old repo, added core.medwave.php and made TODO's within it.

---
+++ system/core.medwave.php
@@ -0,0 +1,25 @@
+<?php
+/******************************\
+ MedWave | Waves for the Future
+ Version 1.0 - AlphaWave
+ Written By: Jeremy Smereka
+	     Amir Salimi
+ ------------------------------
+ core.medwave.php
+ Provides autload functionality
+ and determines which routes to
+ use in the system when certain
+ routes are being accessed.
+\******************************/
+
+namespace MedWave\Core {
+
+	class System {
+		
+		##TODO: Add Autoload Methods into the Class
+		##TODO: Add Database Connection Methods to Class
+		##TODO: Add Routing Methods into Class
+		
+	}
+
+}
