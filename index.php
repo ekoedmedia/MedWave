2836ebf683d45d40d45f4232c278312dae8f0387
Jeremy Smereka <ekeodmedia@gmail.com>
2013-03-03 12:25:53 -0700

    Removed old System Files and updated Index with Autoload Function

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
 
-require('system/core.medwave.php');
+function autoload($className)
+{
+	$className = ltrim($className, '\\');
+	$fileName = '';
+	$namespace = '';
+	if ($lastNsPos = strripos($className, '\\')) {
+		$namespace = substr($className, 0, $lastNsPos);
+		$className = substr($className, $lastNsPos + 1);
+		$fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+	}
+	$fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className).'.php';
+	require $fileName;
+}
+
+spl_autoload_register('autoload');
+
 $core = new MedWave\Core\System();
 $db = $core->getDbcon();
 
