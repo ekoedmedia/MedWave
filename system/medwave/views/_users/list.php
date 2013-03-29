5aef13a45be2f34a7fee2d90bb96ee99b7dbe259
imilas <asalimi@ualberta.ca>
2013-03-29 14:03:23 -0600

    changes to user management

--- system/medwave/views/_users/list.php
+++ system/medwave/views/_users/list.php
@@ -14,12 +14,14 @@ $stmt->execute();
 		<th>Username</th><th>First Name</th><th>Last Name</th><th>Role</th><th>Controls</th>
 	</tr>
 	<?php 
+		$rowNum=0;
 		while ($result = $stmt->fetch(\PDO::FETCH_LAZY)) {
             
-			print "<tr>";
-				print "<td><div contenteditable>".$result->username."</div></td>";
-				print "<td><div contenteditable>".$result->fname."</div></td>";
-				print "<td><div contenteditable>".$result->lname."</div></td>";
+			print "<tr id=".$rowNum.">";
+			
+				print "<td><div id=\"userName".$rowNum."\" contenteditable>".$result->username."</div></td>";
+				print "<td><div id=\"fName".$rowNum."\"contenteditable>".$result->fname."</div></td>";
+				print "<td><div id=\"lName".$rowNum."\"contenteditable>".$result->lname."</div></td>";
 				$selected0="";
 				$selected1="";
 				$selected2="";
@@ -43,7 +45,7 @@ $stmt->execute();
 						break;
 				}
 
-				print "<td><select id=role-dropdown>
+				print "<td><select id=\"role-dropdown".$rowNum."\">
 							<option ".$selected0.">Admin</option>
 							<option ".$selected1." >Doctor</option>
 							<option ".$selected2." >Patient</option>
@@ -52,8 +54,8 @@ $stmt->execute();
 
 				print "<td class=\"user-management-controls\">
 					   
-					   	<form method=\"post\" 
-					      onsubmit=\"GetCellValues()\" class=\"update-user-form\">
+					   	<form method=\"post\" id=\"updateUser\"
+					      onsubmit=\"wat(".$rowNum.")\" class=\"update-user-form\">
 					      
 					          <input type=\"submit\" class=\"update-user-icon\" value=\"\">
 					          <input type=\"hidden\" name=\"user\" value=\"".$result->username."\">
@@ -70,28 +72,26 @@ $stmt->execute();
 					      </form>
 					   </td>";
 			print "</tr>";
+		$rowNum++;
 		}
 	?>
-</table>		
-					<script type="text/javascript">
-				    function GetCellValues() {
-				        var table = document.getElementById('usertable');
-				        for (var r = 0, n = table.rows.length; r < n; r++) {
-				        	print "asd";
-				        	die();
-				                alert(table.rows[r].cells[c].innerHTML);
-				        }
-				    }
-					function setContents();
-					{
-					document.getElementById("role-dropdown").selectedIndex=$rIndex;
-					
-					}
+	<p id="demo">My First Paragraph.</p>
 
-					function getContents();
-					{
-					var x=document.getElementById("role-dropdown").selectedIndex;
-					
-					}
-				</script>
+<script>
+function wat(r){
+						var userName = document.getElementById("userName"+r);					
+						var fName = document.getElementById("fName"+r);
+						var lName = document.getElementById("lName"+r);
+						var role = document.getElementById("role-dropdown"+r);
+						allvars=userName.innerHTML+" "+fName.innerHTML+" "+lName.innerHTML+" "+role.options[role.selectedIndex].value;
+						alert(allvars);
+				
+
+}				        
+
+
+
+</script>
+
+</table>		
 <?php ##TODO: PUT IN PAGINATION LINKS ?>
